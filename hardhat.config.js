1b435bbc52ed921e2d98783266e5055f1d12f54b
Aravinth Ramanathan <30864291+malaipambu@users.noreply.github.com>
2023-03-03 14:00:24 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,7 @@ require("dotenv").config()
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || ""
 const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "0x0000000000000000000000000000000000000000000000000000000000000000"
 
 module.exports = {
     solidity: "0.8.17",
@@ -26,7 +26,7 @@ module.exports = {
         ganache: {
             url: "http://localhost:8545",
             chainId: 1337,
-            accounts: ["0xb253197ad0381f4d72282edf8a60528f94513066d3c3cdb5cc81a732bdcc5984"],
+            //accounts: [],
             developmentChain: true,
         },
         goerli: {
